f78fc6a874fca3f379c4e3ec8c32698bf6a4d2e0
Entao <entaoyang@163.com>
2025-11-24 16:46:49 +0800

    2.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 group = "io.github.yangentao"
 
-version = "2.1.2"
+version = "2.1.3"
 val artifactName = "hare"
 val githubLib = "hare"
 val descLib = "Java/Kotlin Annonations."
@@ -38,7 +38,7 @@ dependencies {
     api("io.github.yangentao:xlog:[2.0.1,)")
     api("io.github.yangentao:anno:[2.0.0,)")
     api("io.github.yangentao:kson:[2.0.7,)")
-    api("io.github.yangentao:types:[2.0.21,)")
+    api("io.github.yangentao:types:[2.0.22,)")
     api("io.github.yangentao:sql:[2.1.2,)")
 
     api("io.github.yangentao:charcode:[2.0.0,)")
